b979e818dfa26412e4e47f282074306f7235e05c
sakshi khare <sakshikhare2006@gmail.com>
2025-09-21 18:46:28 +0530

    patterns

---
+++ lect 8/8.1.c
@@ -0,0 +1,40 @@
+//patterns
+#include<stdio.h>
+int main(){
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 3; k >i; k--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+
+}
+//
+#include<stdio.h>
+int main(){
+    
+    int num=65;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j <=i; j++)
+        {
+            printf(" ");
+
+        }
+        for (int k = 3; k >i; k--)
+        {
+            printf("*");
+        }
+            for(int p=3;p>=i;p--){
+            printf("*");
+     }
+     printf("\n");
+    }
+}
